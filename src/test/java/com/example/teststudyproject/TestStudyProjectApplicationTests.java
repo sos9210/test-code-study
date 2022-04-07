5d3059a247de789c363b0543db46b175f7fccef9
sungsanghyun <sos9215@gmail.com>
2022-04-07 23:35:54 +0900

    최초커밋

---
+++ src/test/java/com/example/teststudyproject/TestStudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teststudyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStudyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
