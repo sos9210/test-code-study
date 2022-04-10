2fedd67e9dbd52c8392b7a9d967f87e036fb1837
sungsanghyun <sos9210@naver.com>
2022-04-10 22:19:24 +0900

    테스트반복 1,2부, 테스트 인스턴스, 테스트 순서, junit-platform.properties

--- src/main/java/com/example/teststudyproject/Study.java
+++ src/main/java/com/example/teststudyproject/Study.java
@@ -1,6 +1,10 @@
 package com.example.teststudyproject;
 
 public class Study {
+    public Study(int limit, String name){
+        this.limit = limit;
+        this.name = name;
+    }
     public Study(int limit) {
         if(limit < 0){
             throw new IllegalArgumentException("limit은 0보다 작으면 IllegalArgumentException");
@@ -12,8 +16,19 @@ public class Study {
 
     private int limit;
 
+    private String name;
+
     public int getLimit() {
         return limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 
     public StudyStatus getStatus() {
