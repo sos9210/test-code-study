b77b10857b35e66cf37bd199cb2a01d8e52de9c2
sungsanghyun <sos9210@naver.com>
2022-06-27 01:29:27 +0900

    컨테이너오류 수정

--- src/main/java/com/example/step2/study/StudyService.java
+++ src/main/java/com/example/step2/study/StudyService.java
@@ -28,7 +28,7 @@ public class StudyService {
         }
         Study newstudy = repository.save(study);
         memberService.notify(newstudy);
-        memberService.notify(member.get());
+        //memberService.notify(member.get());
         return newstudy;
     }
 
